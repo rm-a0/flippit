47ec501d59fcbc4ef741f677ca6ec1126e5a7889
Elena Ivanova <elena.eev0@gmail.com>
2025-10-21 21:09:26 +0200

    Added CardDetailModel and CardListModel

---
+++ src/Common/Flippit.Common.Models/Card/CardListModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flippit.Common.Enums;
+
+namespace Flippit.Common.Models.Card
+{
+    public record CardListModel : IWithId
+    {
+        public Guid Id { get; init; }
+        public required QAType QuestionType { get; set; }
+        public required string Question { get; set; }
+        public string? Description { get; set; }
+    }
+}
