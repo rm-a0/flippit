c7fe7b107e6eaae5fb07c99e07664f22a99ada14
Elena Ivanova <elena.eev0@gmail.com>
2025-10-21 23:00:00 +0200

    Added mappers for updating entities

---
+++ src/Api/Flippit.Api.DAL.Common/Mappers/CardMapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flippit.Api.DAL.Common.Entities;
+using Riok.Mapperly.Abstractions;
+
+namespace Flippit.Api.DAL.Common.Mappers
+{
+    [Mapper]
+    public partial class CardMapper
+    {
+        [MapperIgnoreSource(nameof(CardEntity.Id))]
+        public partial void UpdateEntity(CardEntity source, CardEntity destination);
+    }
+}
