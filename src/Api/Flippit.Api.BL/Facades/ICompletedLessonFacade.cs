267e4ed43f20ca08c7ca7175690d66405b76f567
rm-a0 <michalrepcik9@gmail.com>
2025-10-22 16:11:51 +0200

    feat: implement basic strucure for facades, add IAppFacade and create example CardFacade

---
+++ src/Api/Flippit.Api.BL/Facades/ICompletedLessonFacade.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using Flippit.Common.BL.Facades;
+using Flippit.Common.Models;
+using Flippit.Common.Models.Card;
+
+namespace Flippit.Api.BL.Facades
+{
+    public interface ICompletedLessonFacade : IAppFacade
+    {
+        ///TODO
+    }
+}
