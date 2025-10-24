4404ac81e3c0a8f286ad88ed5b1c098e54e8493a
Elena Ivanova <elena.eev0@gmail.com>
2025-10-24 16:06:15 +0200

    add user endpoints, service registration, change UserDetailModel validation

--- src/Api/Flippit.Api.App/Program.cs
+++ src/Api/Flippit.Api.App/Program.cs
@@ -1,41 +1,162 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Flippit.Api.BL.Facades;
+using Flippit.Api.BL.Installers;
+using Flippit.Api.BL.Mappers;
+using Flippit.Api.DAL.Memory.Installers;
+using Flippit.Common.Models.Card;
+using Flippit.Common.Models.Collection;
+using Flippit.Common.Models.CompletedLesson;
+using Flippit.Common.Models.User;
+using FluentValidation;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+var DALinstaller = new ApiDALMemoryInstaller();
+DALinstaller.Install(builder.Services);
+
+builder.Services.AddSingleton<UserMapper>();
+builder.Services.AddSingleton<CardMapper>();
+builder.Services.AddSingleton<CollectionMapper>();
+builder.Services.AddSingleton<CompletedLessonMapper>();
+
+var BLInstaller = new ApiBLInstaller();
+BLInstaller.Install(builder.Services);
+
+builder.Services.AddScoped<IValidator<UserDetailModel>, UserDetailModelValidator>();
+builder.Services.AddScoped<IValidator<CardDetailModel>, CardDetailModelValidator>();
+builder.Services.AddScoped<IValidator<CollectionDetailModel>, CollectionDetailModelValidator>();
+
+
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.Converters.Add(new JsonStringEnumConverter(JsonNamingPolicy.CamelCase, allowIntegerValues: false));
+    options.SerializerOptions.PropertyNameCaseInsensitive = true;
+});
+
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapOpenApi();
+    app.UseSwagger();
+    //app.UseOpenApi();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+UseEndpoints(app);
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+void UseEndpoints(WebApplication application)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    var endpointsBase = application.MapGroup("api")
+        .WithOpenApi();
+
+    UseUserEndpoints(endpointsBase);
 }
+
+void UseUserEndpoints(RouteGroupBuilder routeGroupBuilder)
+{
+    var userEndpoints = routeGroupBuilder.MapGroup("users")
+        .WithTags("users");
+
+    userEndpoints.MapGet("", ([FromServices] IUserFacade userFacade, [FromQuery] string? filter = null, [FromQuery] int page = 1, [FromQuery] int pageSize = 10, [FromQuery] string? sortBy = null) => {
+        var users = userFacade.GetAll(filter, sortBy, page, pageSize);
+        return TypedResults.Ok(users);
+    });
+
+    userEndpoints.MapGet("/{id:guid}",
+        Results<Ok<UserDetailModel>, NotFound<string>> (
+            Guid id,
+            [FromServices] IUserFacade userFacade
+        )
+        =>
+        userFacade.GetById(id) is { } user
+            ? TypedResults.Ok(user)
+            : TypedResults.NotFound($"User with ID {id} was not found.")
+    );
+
+    userEndpoints.MapPost("", async Task<Results<Ok<Guid>, ValidationProblem>> ([FromBody] UserDetailModel model, IUserFacade userFacade, IValidator<UserDetailModel> validator) => {
+        var validationErrors = await ValidateModelAsync(model, validator);
+        if (validationErrors != null)
+        {
+            return TypedResults.ValidationProblem(validationErrors);
+        }
+        var id = userFacade.Create(model);
+        return TypedResults.Ok(id);
+    });
+
+    userEndpoints.MapPut("", async Task<Results<Ok<Guid?>, ValidationProblem>> ([FromBody] UserDetailModel model, IUserFacade userFacade, IValidator<UserDetailModel> validator) => {
+        var validationErrors = await ValidateModelAsync(model, validator);
+        if (validationErrors != null)
+        {
+            return TypedResults.ValidationProblem(validationErrors);
+        }
+        var id = userFacade.Update(model);
+        return TypedResults.Ok(id);
+    });
+
+    userEndpoints.MapPut("upsert", async Task<Results<Ok<Guid>, ValidationProblem>> ([FromBody] UserDetailModel model, IUserFacade userFacade, IValidator<UserDetailModel> validator) => {
+        var validationErrors = await ValidateModelAsync(model, validator);
+        if (validationErrors != null)
+        {
+            return TypedResults.ValidationProblem(validationErrors);
+        }
+        var id = userFacade.CreateOrUpdate(model);
+        return TypedResults.Ok(id);
+    });
+
+    userEndpoints.MapDelete("{id:guid}", (Guid id, IUserFacade userFacade) =>
+    {
+        userFacade.Delete(id);
+    });
+
+}
+void UseCardEndpoints(RouteGroupBuilder routeGroupBuilder)
+{
+
+}
+void UseCollectionEndpoints(RouteGroupBuilder routeGroupBuilder)
+{
+
+}
+void UseCompletedLessonEndpoints(RouteGroupBuilder routeGroupBuilder)
+{
+
+}
+
+async Task<Dictionary<string, string[]>?> ValidateModelAsync<T>(T model, IValidator<T> validator)
+{
+    var validationResult = await validator.ValidateAsync(model);
+
+    if (validationResult.IsValid)
+    {
+        return null;
+    }
+
+    return validationResult.Errors
+        .GroupBy(e => e.PropertyName)
+        .ToDictionary(
+            g => g.Key,
+            g => g.Select(e => e.ErrorMessage).ToArray()
+        );
+}
+
+
+
+
+
+
+
+
+
