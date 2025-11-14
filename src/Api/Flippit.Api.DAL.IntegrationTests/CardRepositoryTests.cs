956512fdc5357aa062096300b02d5256c9afedd8
rm-a0 <michalrepcik9@gmail.com>
2025-11-14 13:23:31 +0100

    fix: card repository tests

--- src/Api/Flippit.Api.DAL.IntegrationTests/CardRepositoryTests.cs
+++ src/Api/Flippit.Api.DAL.IntegrationTests/CardRepositoryTests.cs
@@ -1,121 +1,99 @@
 ﻿using Flippit.Common.Enums;
 using Flippit.Api.DAL.Common.Entities;
+using Xunit;
+using System.Linq;
+using Flippit.Api.DAL.Common.Repositories;
 
 namespace Flippit.Api.DAL.IntegrationTests;
 
 public class CardRepositoryTests : IClassFixture<InMemoryTestDataProvider>
 {
-    private readonly InMemoryTestDataProvider database;
+    private readonly InMemoryTestDataProvider _database;
+    private readonly ICardRepository _repository;
 
     public CardRepositoryTests(InMemoryTestDataProvider database)
     {
-        this.database = database;
-        database.ResetStorage();
+        _database = database;
+        _database.ResetStorage();
+        _repository = database.GetCardRepository();
     }
 
     [Fact]
     public void GetById_ExistingCard_ReturnsCard()
     {
-        var repository = database.GetCardRepository();
-        var cardId = database.CardGuids[0];
-
-        var card = database.GetCardDirectly(cardId);
-        var result = repository.GetById(cardId);
+        var cardId = _database.CardGuids[0];
+        var card = _database.GetCardDirectly(cardId);
+        var result = _repository.GetById(cardId);
 
         Assert.NotNull(result);
         Assert.Equal(card, result);
         Assert.Equal(cardId, result.Id);
     }
-    
+
     [Fact]
     public void GetById_NonExistingCard_ReturnsNull()
     {
-        var repository = database.GetCardRepository();
-        var nonExistingId = Guid.NewGuid();
-
-        var result = repository.GetById(nonExistingId);
-
+        var result = _repository.GetById(Guid.NewGuid());
         Assert.Null(result);
     }
 
     [Fact]
     public void GetAll_ReturnsAllCards()
     {
-        var repository = database.GetCardRepository();
-
-        var result = repository.GetAll();
-
+        var result = _repository.GetAll();
         Assert.Equal(3, result.Count);
     }
-    
+
     [Fact]
     public void GetAll_WithPagination_ReturnsPagedResults()
     {
-        var repository = database.GetCardRepository();
-
-        var result = repository.GetAll(null, null, 1, 2);
-
+        var result = _repository.GetAll(null, null, 1, 2);
         Assert.Equal(2, result.Count);
     }
-    
+
     [Fact]
     public void Search_ReturnsCorrectCards()
     {
-        var repository = database.GetCardRepository();
-        
-        var result = repository.Search("What");
-        
+        var result = _repository.Search("What");
+
         Assert.NotNull(result);
-        Assert.Contains(result, c => c.Id == database.CardGuids[0]);
-        Assert.Contains(result, c => c.Id == database.CardGuids[1]);
-        Assert.Contains(result, c => c.Id == database.CardGuids[2]);
+        Assert.Contains(result, c => c.Id == _database.CardGuids[0]);
+        Assert.Contains(result, c => c.Id == _database.CardGuids[1]);
+        Assert.Contains(result, c => c.Id == _database.CardGuids[2]);
     }
-    
+
     [Fact]
     public void Search_NotFound_ReturnsEmpty()
     {
-        var repository = database.GetCardRepository();
-        
-        var result = repository.Search("Java");
-        
+        var result = _repository.Search("Java");
         Assert.Empty(result);
     }
-    
+
     [Fact]
     public void SearchByCreatorId_ReturnsCorrectCards()
     {
-        var repository = database.GetCardRepository();
-        var creatorId = database.UserGuids[0];
-        var wantedCardId = database.CardGuids[0];
-        
-        var result = repository.SearchByCreatorId(creatorId);
-        
+        var creatorId = _database.UserGuids[0];
+        var result = _repository.SearchByCreatorId(creatorId);
+
         Assert.NotNull(result);
-        Assert.Equal(result.First().Id, wantedCardId);
+        Assert.Equal(_database.CardGuids[0], result.First().Id);
     }
-    
+
     [Fact]
     public void SearchByCollectionId_ReturnsCorrectCards()
     {
-        var repository = database.GetCardRepository();
-        var collectionId = database.CollectionGuids[0];
-        
-        var result = repository.SearchByCollectionId(collectionId);
-        
-        var wantedCardId = database.CardGuids[0];
-        
-        Assert.NotNull(result);
+        var collectionId = _database.CollectionGuids[0];
+        var result = _repository.SearchByCollectionId(collectionId);
+
         Assert.Equal(2, result.Count());
-        Assert.Contains(result, c => c.Id == wantedCardId);
+        Assert.Contains(result, c => c.Id == _database.CardGuids[0]);
         Assert.Contains(result, c => c.Question == "What is C#?");
     }
 
     [Fact]
     public void Insert_NewCard_CardIsInserted()
     {
-        var repository = database.GetCardRepository();
         var newCardId = Guid.NewGuid();
-
         var newCard = new CardEntity
         {
             Id = newCardId,
@@ -123,51 +101,49 @@ public class CardRepositoryTests : IClassFixture<InMemoryTestDataProvider>
             Question = "Test Question",
             AnswerType = QAType.Text,
             Answer = "Test Answer",
-            CollectionId = database.CollectionGuids[0],
-            CreatorId = database.UserGuids[0]
+            CollectionId = _database.CollectionGuids[0],
+            CreatorId = _database.UserGuids[0]
         };
 
-        repository.Insert(newCard);
+        _repository.Insert(newCard);
 
-        var insertedCard = database.GetCardDirectly(newCardId);
+        var insertedCard = _database.GetCardDirectly(newCardId);
+
         Assert.NotNull(insertedCard);
+        Assert.Equal(newCardId, insertedCard.Id);
         Assert.Equal("Test Question", insertedCard.Question);
+        Assert.Equal("Test Answer", insertedCard.Answer);
     }
-    
+
     [Fact]
     public void Update_ExistingCard_CardIsUpdated()
     {
-        var repository = database.GetCardRepository();
-        var cardId = database.CardGuids[0];
-        
-        var card = repository.GetById(cardId);
+        var cardId = _database.CardGuids[0];
+        var card = _repository.GetById(cardId);
         Assert.NotNull(card);
-        
-        var updatedCard = new CardEntity()
+
+        var updatedCard = new CardEntity
         {
             Id = cardId,
             QuestionType = QAType.Text,
             Question = "Updated question",
             AnswerType = QAType.Text,
             Answer = "Updated answer",
-            CreatorId = database.UserGuids[0],
-            CollectionId = database.CollectionGuids[0]
+            CreatorId = _database.UserGuids[0],
+            CollectionId = _database.CollectionGuids[0]
         };
-        
-        repository.Update(updatedCard);
-        
-        var result = database.GetCardDirectly(cardId);
-        Assert.NotNull(result);
-        Assert.Equal("Updated question", result.Question);
+
+        _repository.Update(updatedCard);
+
+        var result = _database.GetCardDirectly(cardId);
+        Assert.Equal("Updated question", result!.Question);
         Assert.Equal("Updated answer", result.Answer);
     }
-    
+
     [Fact]
     public void Insert_NonExistingCard_CardIsInserted()
     {
-        var repository = database.GetCardRepository();
         var cardId = Guid.NewGuid();
-        
         var newCard = new CardEntity
         {
             Id = cardId,
@@ -175,50 +151,39 @@ public class CardRepositoryTests : IClassFixture<InMemoryTestDataProvider>
             Question = "Inserted question",
             AnswerType = QAType.Text,
             Answer = "Inserted answer",
-            CreatorId = database.UserGuids[0],
-            CollectionId = database.CollectionGuids[0]
+            CreatorId = _database.UserGuids[0],
+            CollectionId = _database.CollectionGuids[0]
         };
-        
-        repository.Insert(newCard);
-        
-        var result = database.GetCardDirectly(cardId);
-        
+
+        _repository.Insert(newCard);
+
+        var result = _database.GetCardDirectly(cardId);
         Assert.NotNull(result);
         Assert.Equal("Inserted question", result.Question);
     }
-    
+
     [Fact]
     public void Delete_ExistingCard_CardIsDeleted()
     {
-        var repository = database.GetCardRepository();
-        var cardToDeleteId = database.CardGuids[0];
-        
-        repository.Remove(cardToDeleteId);
-        
-        var result = database.GetCardDirectly(cardToDeleteId);
-        
+        var cardToDeleteId = _database.CardGuids[0];
+
+        _repository.Remove(cardToDeleteId);
+
+        var result = _database.GetCardDirectly(cardToDeleteId);
         Assert.Null(result);
     }
-    
+
     [Fact]
     public void Exists_ExistingCard_ReturnsTrue()
     {
-        var repository = database.GetCardRepository();
-        var cardId = database.CardGuids[0];
-        
-        var result = repository.Exists(cardId);
-        
+        var result = _repository.Exists(_database.CardGuids[0]);
         Assert.True(result);
     }
-    
+
     [Fact]
     public void Exists_NonExistentCard_ReturnsFalse()
     {
-        var repository = database.GetCardRepository();
-        var cardId = Guid.Parse("00000000-0000-0000-0000-000000000000");
-        
-        var result = repository.Exists(cardId);
-        
+        var result = _repository.Exists(Guid.Parse("00000000-0000-0000-0000-000000000000"));
         Assert.False(result);
     }
 }
