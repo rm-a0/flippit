ee3d2597939512318d17b25470d7cca1a3c95e6f
rm-a0 <michalrepcik9@gmail.com>
2025-11-15 15:40:40 +0100

    fix: completted lesson controller test

--- src/Api/Flippit.Api.App.EndToEndTests/CompletedLessonControllerTests.cs
+++ src/Api/Flippit.Api.App.EndToEndTests/CompletedLessonControllerTests.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Json;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Flippit.Common.Models.CompletedLesson;
 using Flippit.Common.Models.User;
@@ -13,11 +15,17 @@ namespace Flippit.Api.App.EndToEndTests
     {
         private readonly FlippitApiApplicationFactory application;
         private readonly Lazy<HttpClient> client;
+        private readonly JsonSerializerOptions jsonOptions;
 
         public CompletedLessonControllerTests()
         {
             application = new FlippitApiApplicationFactory();
             client = new Lazy<HttpClient>(application.CreateClient());
+            jsonOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase, allowIntegerValues: false) }
+            };
         }
 
         [Fact]
@@ -65,43 +73,44 @@ namespace Flippit.Api.App.EndToEndTests
         }
 
         [Fact]
-        public async Task GetCompletedLessonsOfCollection_return_only_one()
+        public async Task GetCompletedLessonsOfCollection_Returns_Only_Lessons_From_That_Collection()
         {
-            var collectionId = Guid.NewGuid();
+            var targetCollectionId = Guid.NewGuid();
+            var otherCollectionId = Guid.NewGuid();
 
-            var CompletedLesson1 = new CompletedLessonDetailModel
+            var lessonInTarget = new CompletedLessonDetailModel
             {
                 Id = Guid.NewGuid(),
                 UserId = Guid.NewGuid(),
                 AnswersJson = "{}",
                 StatisticsJson = "{}",
-                CollectionId = collectionId,
-
+                CollectionId = targetCollectionId
             };
 
-            var lesson1response = await client.Value.PostAsJsonAsync("/api/completedLessons", CompletedLesson1);
-            lesson1response.EnsureSuccessStatusCode();
+            var post1 = await client.Value.PostAsJsonAsync("/api/completedLessons", lessonInTarget, jsonOptions);
+            post1.EnsureSuccessStatusCode();
 
-            var CompletedLesson2 = new CompletedLessonDetailModel
+            var lessonInOther = new CompletedLessonDetailModel
             {
                 Id = Guid.NewGuid(),
                 UserId = Guid.NewGuid(),
                 AnswersJson = "{}",
                 StatisticsJson = "{}",
-                CollectionId = Guid.NewGuid(),
-
+                CollectionId = otherCollectionId
             };
+            var post2 = await client.Value.PostAsJsonAsync("/api/completedLessons", lessonInOther, jsonOptions);
+            post2.EnsureSuccessStatusCode();
 
-            var lesson2response = await client.Value.PostAsJsonAsync("/api/completedLessons", CompletedLesson2);
-            lesson2response.EnsureSuccessStatusCode();
-
-            var response = await client.Value.GetAsync($"api/collections/{collectionId}/completedLessons");
+            var response = await client.Value.GetAsync($"/api/collections/{targetCollectionId}/completedLessons");
             response.EnsureSuccessStatusCode();
+            var lessonsInCollection = await response.Content.ReadFromJsonAsync<IEnumerable<CompletedLessonListModel>>(jsonOptions);
 
-            var userCompletedLessons = await response.Content.ReadFromJsonAsync<IEnumerable<CompletedLessonListModel>>();
-            Assert.NotEmpty(userCompletedLessons!);
-            Assert.Single(userCompletedLessons!);
-        }
+            Assert.NotNull(lessonsInCollection);
+            Assert.NotEmpty(lessonsInCollection);
+            Assert.Single(lessonsInCollection);
+            Assert.Equal(targetCollectionId, lessonsInCollection.First().CollectionId);
+        } 
+
         public async ValueTask DisposeAsync()
         {
             await application.DisposeAsync();
