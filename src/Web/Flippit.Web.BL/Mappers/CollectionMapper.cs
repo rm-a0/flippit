5d0606feafcaf5f71a0ac264030bd0f5bc9381e2
Elena Ivanova <elena.eev0@gmail.com>
2025-12-10 12:55:49 +0100

    generated apiClient

--- src/Web/Flippit.Web.BL/Mappers/CollectionMapper.cs
+++ src/Web/Flippit.Web.BL/Mappers/CollectionMapper.cs
@@ -8,8 +8,6 @@ namespace Flippit.Web.BL.Mappers
     public partial class CollectionMapper
     {
         [MapperIgnoreSource(nameof(CollectionDetailModel.CreatorId))]
-        [MapperIgnoreSource(nameof(CollectionDetailModel.StartTime))]
-        [MapperIgnoreSource(nameof(CollectionDetailModel.EndTime))]
         public partial CollectionListModel DetailToListModel(CollectionDetailModel detail);
 
         public partial IList<CollectionListModel> DetailToListModels(IEnumerable<CollectionDetailModel> details);
