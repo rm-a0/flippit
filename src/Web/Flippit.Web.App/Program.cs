c42d520b9c6862cacb8f2b17140e7fd6a9218aa6
Elena Ivanova <elena.eev0@gmail.com>
2025-12-10 14:00:13 +0100

    added localdboptions

--- src/Web/Flippit.Web.App/Program.cs
+++ src/Web/Flippit.Web.App/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Flippit.Web.App;
+using Flippit.Web.BL;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped<UserState>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7175") });
+
+builder.Services.AddSingleton(new LocalDbOptions { IsLocalDbEnabled = false });
 
 await builder.Build().RunAsync();
